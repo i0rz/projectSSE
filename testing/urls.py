52ef0be953fd526edc804a4692b100ca12597549
viren <pavithren@mymail.sutd.edu.sg>
2019-04-19 16:39:03 +0800

    merged django server config with Jacob's version and renamed folders from Hello to Testing.

---
+++ testing/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+from .models import LogMessage
+from django.contrib import admin
+
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:10],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="testing/home.html",
+)
+
+urlpatterns = [
+    path("", home_list_view, name="home"),
+    path("about/",views.about, name="about"),
+    path("contact/",views.contact, name="contact"),
+    path("testing/<name>", views.hello_there, name="hello_there"),
+    path("log/", views.log_message, name="log"),
+
+    #first arg is a route "hello/" that accepts a variable string called name,
+    #the string is passed to the views.hello_there
+]
+
--- testing/views.py
+++ testing/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import LogMessageForm
+from .models import LogMessage
+from django.views.generic import ListView
+import re
 
+from datetime import datetime
 # Create your views here.
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+def about(request):
+    return render(request, "testing/about.html")
+
+def contact(request):
+    return render(request, "testing/contact.html")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "testing/log_message.html", {"form": form})
+
+
+def hello_there(request, name):
+    return render(
+        request,
+        'testing/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+    # now = datetime.now()
+    # formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # # Filter the name argument to letters only using regular expressions. URL arguments
+    # # can contain arbitrary text, so we restrict to safe characters only.
+    # match_object = re.match("[a-zA-Z]+", name)
+
+    # if match_object:
+    #     clean_name = match_object.group(0)
+    # else:
+    #     clean_name = "Friend"
+
+    # # content = "Hello there, " + clean_name + "! It's " + formatted_now
+    # content = "<h1>Hello there, " + clean_name + "! It's "+formatted_now+"</h1>"
+    # return HttpResponse(content)
